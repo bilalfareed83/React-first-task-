e9c082daa518fe51af9c0a6205f022d5b78a8592
bilalfareed83 <bilal_fareed@hotmail.com>
2020-02-21 15:28:01 +0500

    return image but not return data

--- src/Components/CardData/cardData.component.jsx
+++ src/Components/CardData/cardData.component.jsx
@@ -3,19 +3,11 @@ import Card from '../Card/card.component';
 
 class CardData extends Component {
   render() {
-    console.log(this.props.user);
+    // console.log(this.props);
     return (
       <div>
         {this.props.user.map((item, i) => {
-          return (
-            this.props.user.name,
-            (
-              <Card
-                imageUrl={this.props.imageUrl}
-                decription={this.props.decription}
-              />
-            )
-          );
+          return <Card key={i}></Card>;
         })}
       </div>
     );
